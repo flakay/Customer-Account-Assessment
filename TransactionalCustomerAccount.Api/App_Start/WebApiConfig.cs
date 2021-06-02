0f0a3df617c9940abf304b7c7ce7ae0a39c31217
Fabian Lakay <fabianlakay@gmail.com>
2021-06-02 22:33:55 +0200

    Customer Transaction Assessment
    Branch creation and commit

---
+++ TransactionalCustomerAccount.Api/App_Start/WebApiConfig.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Http;
+using System.Web.Http.Cors;
+
+namespace TransactionalCustomerAccount.Api
+{
+    public static class WebApiConfig
+    {
+        public static void Register(HttpConfiguration config)
+        {
+            // Web API configuration and services
+     
+            var cors = new EnableCorsAttribute("*", "*", "*");
+            config.EnableCors(cors);
+
+            // Web API routes
+            config.MapHttpAttributeRoutes();
+            config.Routes.MapHttpRoute(
+ name: "TestApi",//route name
+ routeTemplate: "api/{controller}/{action}/{id}",//route pattern
+ defaults: new { id = RouteParameter.Optional }//parameter default values
+ );
+            config.Routes.MapHttpRoute(
+                name: "DefaultApi",
+                routeTemplate: "api/{controller}/{id}",
+                defaults: new { id = RouteParameter.Optional }
+            );
+        }
+    }
+}
